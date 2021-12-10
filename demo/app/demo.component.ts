20e3e2a3de8f5a56cd85f67078715a1b5426ad3c
thoussem <t.houssemg@gmail.com>
2021-12-10 13:15:00 +0100

    stackblitz

--- demo/app/demo.component.ts
+++ demo/app/demo.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 import { Examples } from './example-schemas.model';
 import { JsonPointer } from '@ajsf/core';
+import * as SCHEMA from './schema.json';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -14,9 +21,7 @@ import { JsonPointer } from '@ajsf/core';
   animations: [
     trigger('expandSection', [
       state('in', style({ height: '*' })),
-      transition(':enter', [
-        style({ height: 0 }), animate(100),
-      ]),
+      transition(':enter', [style({ height: 0 }), animate(100)]),
       transition(':leave', [
         style({ height: '*' }),
         animate(100, style({ height: 0 })),
@@ -28,15 +33,20 @@ export class DemoComponent implements OnInit {
   examples: any = Examples;
   languageList: any = ['de', 'en', 'es', 'fr', 'it', 'pt', 'zh'];
   languages: any = {
-    'de': 'German',
-    'en': 'English',
-    'es': 'Spanish',
-    'fr': 'French',
-    'it': 'Italian',
-    'pt': 'Portuguese',
-    'zh': 'Chinese'
+    de: 'German',
+    en: 'English',
+    es: 'Spanish',
+    fr: 'French',
+    it: 'Italian',
+    pt: 'Portuguese',
+    zh: 'Chinese',
   };
-  frameworkList: any = ['material-design', 'bootstrap-3', 'bootstrap-4', 'no-framework'];
+  frameworkList: any = [
+    'material-design',
+    'bootstrap-3',
+    'bootstrap-4',
+    'no-framework',
+  ];
   frameworks: any = {
     'material-design': 'Material Design',
     'bootstrap-3': 'Bootstrap 3',
@@ -53,7 +63,7 @@ export class DemoComponent implements OnInit {
     options: true,
     schema: true,
     form: true,
-    output: true
+    output: true,
   };
 
   formActive = false;
@@ -85,35 +95,34 @@ export class DemoComponent implements OnInit {
     private http: HttpClient,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     // Subscribe to query string to detect schema to load
-    this.route.queryParams.subscribe(
-      params => {
-        if (params['set']) {
-          this.selectedSet = params['set'];
-          this.selectedSetName = ({
-            'ng-jsf': '',
-            'asf': 'Angular Schema Form:',
-            'rsf': 'React Schema Form:',
-            'jsf': 'JSONForm:'
-          })[this.selectedSet];
-        }
-        if (params['example']) {
-          this.selectedExample = params['example'];
-          this.selectedExampleName = this.examples[this.selectedSet].schemas
-            .find(schema => schema.file === this.selectedExample).name;
-        }
-        if (params['framework']) {
-          this.selectedFramework = params['framework'];
-        }
-        if (params['language']) {
-          this.selectedLanguage = params['language'];
-        }
-        this.loadSelectedExample();
+    this.route.queryParams.subscribe((params) => {
+      if (params['set']) {
+        this.selectedSet = params['set'];
+        this.selectedSetName = {
+          'ng-jsf': '',
+          asf: 'Angular Schema Form:',
+          rsf: 'React Schema Form:',
+          jsf: 'JSONForm:',
+        }[this.selectedSet];
       }
-    );
+      if (params['example']) {
+        this.selectedExample = params['example'];
+        this.selectedExampleName = this.examples[this.selectedSet].schemas.find(
+          (schema) => schema.file === this.selectedExample
+        ).name;
+      }
+      if (params['framework']) {
+        this.selectedFramework = params['framework'];
+      }
+      if (params['language']) {
+        this.selectedLanguage = params['language'];
+      }
+      this.loadSelectedExample();
+    });
   }
 
   onSubmit(data: any) {
@@ -141,7 +150,9 @@ export class DemoComponent implements OnInit {
   }
 
   get prettyValidationErrors() {
-    if (!this.formValidationErrors) { return null; }
+    if (!this.formValidationErrors) {
+      return null;
+    }
     const errorArray = [];
     for (const error of this.formValidationErrors) {
       const message = error.message;
@@ -166,7 +177,9 @@ export class DemoComponent implements OnInit {
     selectedExample: string = this.selectedExample,
     selectedExampleName: string = this.selectedExampleName
   ) {
-    if (this.menuTrigger.menuOpen) { this.menuTrigger.closeMenu(); }
+    if (this.menuTrigger.menuOpen) {
+      this.menuTrigger.closeMenu();
+    }
     if (selectedExample !== this.selectedExample) {
       this.formActive = false;
       this.selectedSet = selectedSet;
@@ -174,23 +187,29 @@ export class DemoComponent implements OnInit {
       this.selectedExample = selectedExample;
       this.selectedExampleName = selectedExampleName;
       this.router.navigateByUrl(
-        '/?set=' + selectedSet +
-        '&example=' + selectedExample +
-        '&framework=' + this.selectedFramework +
-        '&language=' + this.selectedLanguage
+        '/?set=' +
+          selectedSet +
+          '&example=' +
+          selectedExample +
+          '&framework=' +
+          this.selectedFramework +
+          '&language=' +
+          this.selectedLanguage
       );
       this.liveFormData = {};
       this.submittedFormData = null;
       this.formIsValid = null;
       this.formValidationErrors = null;
     }
-    const exampleURL = `assets/example-schemas/${this.selectedExample}.json`;
-    this.http
-      .get(exampleURL, { responseType: 'text' })
-      .subscribe(schema => {
-        this.jsonFormSchema = schema;
-        this.generateForm(this.jsonFormSchema);
-      });
+    // const exampleURL = `assets/example-schemas/${this.selectedExample}.json`;
+    // this.http
+    //   .get(exampleURL, { responseType: 'text' })
+    //   .subscribe(schema => {
+    //     this.jsonFormSchema = schema;
+    //     this.generateForm(this.jsonFormSchema);
+    //   });
+    this.jsonFormSchema = JSON.stringify(SCHEMA, null, 2);
+    this.generateForm(this.jsonFormSchema);
   }
 
   loadSelectedLanguage() {
@@ -200,7 +219,9 @@ export class DemoComponent implements OnInit {
   // Display the form entered by the user
   // (runs whenever the user changes the jsonform object in the ACE input field)
   generateForm(newFormString: string) {
-    if (!newFormString) { return; }
+    if (!newFormString) {
+      return;
+    }
     this.jsonFormStatusMessage = 'Loading form...';
     this.formActive = false;
     this.liveFormData = {};
@@ -210,13 +231,11 @@ export class DemoComponent implements OnInit {
     // but if an example schema includes a function,
     // it will be compiled it as Javascript instead
     try {
-
       // Parse entered content as JSON
       this.jsonFormObject = JSON.parse(newFormString);
       this.jsonFormValid = true;
     } catch (jsonError) {
       try {
-
         // If entered content is not valid JSON,
         // parse as JavaScript instead to include functions
         const newFormObject: any = null;
@@ -226,13 +245,13 @@ export class DemoComponent implements OnInit {
         this.jsonFormObject = newFormObject;
         this.jsonFormValid = true;
       } catch (javascriptError) {
-
         // If entered content is not valid JSON or JavaScript, show error
         this.jsonFormValid = false;
         this.jsonFormStatusMessage =
           'Entered content is not currently a valid JSON Form object.\n' +
           'As soon as it is, you will see your form here. So keep typing. :-)\n\n' +
-          'JavaScript parser returned:\n\n' + jsonError;
+          'JavaScript parser returned:\n\n' +
+          jsonError;
         return;
       }
     }
